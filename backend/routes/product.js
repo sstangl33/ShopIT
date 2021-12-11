02e004bb3a14d19cace8bfc8297e6354cca4c4c1
sstangl33 <stevestangl@gmail.com>
2021-12-11 14:48:26 -0500

    Add route to delete a product

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -6,6 +6,7 @@ const {
   newProduct,
   getSingleProduct,
   updateProduct,
+  deleteProduct,
 } = require("../controllers/productController");
 
 router.route("/products").get(getProducts);
@@ -13,6 +14,6 @@ router.route("/product/:id").get(getSingleProduct);
 
 router.route("/admin/product/new").post(newProduct);
 
-router.route("/admin/product/:id").put(updateProduct);
+router.route("/admin/product/:id").put(updateProduct).delete(deleteProduct);
 
 module.exports = router;
