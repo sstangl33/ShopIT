6d13e1d9a18ca535eedd8560590966dcad6ef76e
sstangl33 <stephenstangl@gmail.com>
2021-12-11 13:53:05 -0500

    Add a route to update a product.

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -5,11 +5,14 @@ const {
   getProducts,
   newProduct,
   getSingleProduct,
+  updateProduct,
 } = require("../controllers/productController");
 
 router.route("/products").get(getProducts);
 router.route("/product/:id").get(getSingleProduct);
 
-router.route("/product/new").post(newProduct);
+router.route("/admin/product/new").post(newProduct);
+
+router.route("/admin/product/:id").put(updateProduct);
 
 module.exports = router;
